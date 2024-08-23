266f52b8f26ed017d82aa05dec6225474a3273cd
IbrahemHassen <ibrahemmohye@gmail.com>
2024-08-23 19:15:24 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ f1=st.number_input('feature 1',min_value=1,max_value=10)
 f2=st.number_input('feature 2',min_value=1,max_value=10)
 f3=st.number_input('feature 3',min_value=1,max_value=100)
 
-with open('model(1).pkl','rb')as file:
+with open('model2.pkl','rb')as file:
   model=pickle.load(file)
   
 
