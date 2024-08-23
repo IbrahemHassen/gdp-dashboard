972513c38aa7d10b31af2960d345a4d9e1d48b09
IbrahemHassen <ibrahemmohye@gmail.com>
2024-08-23 18:49:41 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit as st
+import pickle
 import pandas as pd
 import math
 from pathlib import Path
 
 st.title('car co2 emssion')
-st.number_input('feature 1')
+f1=st.number_input('feature 1',min_value=1,max_value=10)
+f2=st.number_input('feature 2',min_value=1,max_value=10)
+f3=st.number_input('feature 3',min_value=1,max_value=100)
